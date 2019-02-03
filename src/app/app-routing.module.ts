ae1ef62955632d52ede7cd18c89a07f188f28005
Denis Putnam <denisputnam@verizon.net>
2019-02-03 11:38:26 -0500

    Added the homescreen module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomescreenComponent } from './homescreen/homescreen.component';
+
+const routes: Routes = [
+  { path: '', component: HomescreenComponent },
+  { path: '*', component: HomescreenComponent },
+  // { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomescreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/homescreen/homescreen.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomescreenComponent } from './homescreen.component';
+
+@NgModule({
+  declarations: [HomescreenComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomescreenModule { }
