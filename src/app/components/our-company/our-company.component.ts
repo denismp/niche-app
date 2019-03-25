ee43e9e44f6b26f22b1033e01fcbeb8a4befaf5b
Denis Putnam <denisputnam@verizon.net>
2019-03-24 21:19:43 -0400

    Added the Actions column to the Our Company table

--- src/app/components/our-company/our-company.component.ts
+++ src/app/components/our-company/our-company.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OurCompany } from 'src/app/models/our-company.model';
 import { OurCompanyStore } from 'src/app/stores/our-company-store';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-our-company',
@@ -36,4 +37,15 @@ export class OurCompanyComponent implements OnInit {
     this.ourCompanyStore.getAll$().subscribe(ourCompanys => { this.ourCompanys = ourCompanys; })
   }
 
+  routeToOurProducts(ourCompany: OurCompany): void {
+    this.selectedOurCompany = ourCompany;
+    console.log('routeToOurProducts(): called...');
+    var products: Product[] = ourCompany.company.products;
+    for( var i in products) {
+      console.log("ID=" + products[i].id)
+      console.log("ASIN=" + products[i].asin)
+    }
+    // this.store.dispatch({ type: 'SELECT_AUTHOR', payload: this.selectedAuthor });
+    // this.router.navigate(['/home/authors/detail']);
+  }
 }
