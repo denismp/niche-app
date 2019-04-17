03d53ed09554f0d13c23c9993a9b9be24179d602
Denis Putnam <denisputnam@verizon.net>
2019-04-17 14:59:23 -0400

    formatted the code

--- src/app/homescreen/homescreen.component.ts
+++ src/app/homescreen/homescreen.component.ts
@@ -19,19 +19,19 @@ export class HomescreenComponent implements OnInit {
     this.items = [{
       label: 'Menu', style: "background-color: red",
       items: [
-          {label: 'Home', routerLink: "/home", routerLinkActiveOptions: "active"},
-          {label: 'Our Company', routerLink: "/ourcompany.component", routerLinkActiveOptions: "active"},
-          {label: 'Products', routerLink: "/product.component", routerLinkActiveOptions: "active"},
-          {label: 'Company', routerLink: "/company.component", routerLinkActiveOptions: "active"},
-          {label: 'Contact Information', routerLink: "/contact.info.component", routerLinkActiveOptions: "active"},
-          {label: 'Shipment Information', routerLink: "/shipment.info.component", routerLinkActiveOptions: "active"},
-          {label: 'Parent ASINs', routerLink: "/parent.asin.component", routerLinkActiveOptions: "active"},
-          {label: 'Competitors To Target', routerLink: "/competitors.to.target.component", routerLinkActiveOptions: "active"},
-          {label: 'Checkpoint Criteria', routerLink: "/checkpoint.criteria.component", routerLinkActiveOptions: "active"},
-          {label: 'Criteria Yes Nos', routerLink: "/criteria.yes.no.component", routerLinkActiveOptions: "active"},
-          {label: 'Download', icon: 'pi pi-fw pi-download'}
+        { label: 'Home', routerLink: "/home", routerLinkActiveOptions: "active" },
+        { label: 'Our Company', routerLink: "/ourcompany.component", routerLinkActiveOptions: "active" },
+        { label: 'Products', routerLink: "/product.component", routerLinkActiveOptions: "active" },
+        { label: 'Company', routerLink: "/company.component", routerLinkActiveOptions: "active" },
+        { label: 'Contact Information', routerLink: "/contact.info.component", routerLinkActiveOptions: "active" },
+        { label: 'Shipment Information', routerLink: "/shipment.info.component", routerLinkActiveOptions: "active" },
+        { label: 'Parent ASINs', routerLink: "/parent.asin.component", routerLinkActiveOptions: "active" },
+        { label: 'Competitors To Target', routerLink: "/competitors.to.target.component", routerLinkActiveOptions: "active" },
+        { label: 'Checkpoint Criteria', routerLink: "/checkpoint.criteria.component", routerLinkActiveOptions: "active" },
+        { label: 'Criteria Yes Nos', routerLink: "/criteria.yes.no.component", routerLinkActiveOptions: "active" },
+        { label: 'Download', icon: 'pi pi-fw pi-download' }
       ]
-  }];
+    }];
   }
 
 }
